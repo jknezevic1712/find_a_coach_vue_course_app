b889b206c7c833fff375361da4196544aafa1b30
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-05-19 17:44:20 +0200

    Login implemented.

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,28 @@
 export default {
-  login() {},
+  async login(ctx, payload) {
+    const res = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCsXTvTLGFq24KxVB5hJeNGiUM7Gh4cuTM`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const resData = await res.json();
+    if (!res.ok) {
+      const err = new Error(resData.message || 'Failed to authenticate.');
+      throw err;
+    }
+
+    ctx.commit('setUser', {
+      token: resData.idToken,
+      userID: resData.localId,
+      tokenExpiration: resData.expiresIn,
+    });
+  },
   async signup(ctx, payload) {
     const res = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCsXTvTLGFq24KxVB5hJeNGiUM7Gh4cuTM`,
@@ -12,10 +35,7 @@ export default {
         }),
       }
     );
-
     const resData = await res.json();
-
-    console.log('resData ', resData);
     if (!res.ok) {
       const err = new Error(resData.message || 'Failed to authenticate.');
       throw err;
