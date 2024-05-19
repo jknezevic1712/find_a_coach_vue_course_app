0979a55abe8b9da16b6db966845f20ff0c48023a
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-05-19 09:47:07 +0200

    Not found page added.

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
-// import NotFound from './pages/NotFound.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -38,10 +38,10 @@ const router = createRouter({
       path: '/requests',
       component: RequestsReceived,
     },
-    // {
-    //   path: '/:notFound(.*)',
-    //   component: NotFound,
-    // },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
   ],
 });
 
